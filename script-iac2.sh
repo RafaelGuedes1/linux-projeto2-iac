c50f8a01d4309b8ecc338beec6411bf93671fd74
Rafael <167644827+RafaelGuedes1@users.noreply.github.com>
2024-10-07 16:01:42 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -10,9 +10,9 @@ apt-get install unzip -y
 echo "Baixando e copiando os arquivos da aplicação..."
 
 cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+wget https://github.com/denilsonbonatti/linux-projeto2-iac/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio-main
+cd linux-projeto2-iac-main
 cp -R * /var/www/html/
 
 
